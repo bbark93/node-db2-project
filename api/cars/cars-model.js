6c8c378b4ec9356029eb8f89831c24eb46fd5e9a
Brandon Barksdale <bbark93@gmail.com>
2024-09-08 19:26:05 -0400

    post endpoint finished

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,19 +1,26 @@
-const db = require('../../data/db-config')
+const db = require("../../data/db-config");
 
 const getAll = () => {
-  return db('cars')
-}
+  return db("cars");
+};
 
 const getById = (id) => {
-  return db('cars').where('id', id).first()
-}
+  return db("cars").where("id", id).first();
+};
 
-const create = () => {
-  // DO YOUR MAGIC
-}
+const getByVin = (vin) => {
+  return db("cars").where("vin", vin).first();
+};
+
+const create = (car) => {
+  return db("cars")
+    .insert(car)
+    .then(([id]) => getById(id));
+};
 
 module.exports = {
   getAll,
   getById,
   create,
-}
+  getByVin,
+};

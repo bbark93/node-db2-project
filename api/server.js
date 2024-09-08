2a107f4597dc3d6afaf8e35242331ccb86db8e87
Brandon Barksdale <bbark93@gmail.com>
2024-09-08 14:17:05 -0400

    nodemon installed and debugger wired

--- api/server.js
+++ api/server.js
@@ -5,11 +5,10 @@ const server = express();
 server.use(express.json());
 
 server.use("*", (req, res, next) => {
-  next({ status: 404, message: "not found" });
+  next({ status: 404, message: "not found!" });
 });
 
-server.use((err, req, res, next) => {
-  // eslint-disable-line
+server.use((err, req, res, next) => {// eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
   });
